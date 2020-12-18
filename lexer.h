26e57eae3c69665b6c29c3c40dfee2ac74af9c3b
Davis <46525133+DavisR@users.noreply.github.com>
2020-12-18 13:24:43 +0100

    Update lexer.h

--- lexer.h
+++ lexer.h
@@ -4,11 +4,11 @@
 #include <string>
 
 
-int findNextChar(std::string str, int i);
-std::string tokenizeStr(std::string &str, char atChar);
-bool checkNr(std::string str);
-bool checkOP(std::string str);
-int OPArithmetic(std::string &tok, std::vector<int> &Stack);
+int findNextChar(std::string str, int i); // Get next char of stack
+std::string tokenizeStr(std::string &str, char atChar); // Tokenize given string for stack
+bool checkNr(std::string str); // Check if char is numerical
+bool checkOP(std::string str); // Check if char is an operator
+int OPArithmetic(std::string &tok, std::vector<int> &Stack); // Perform arithmetic using 2 operands and an operator
 
 
 #endif // LEXER_H
